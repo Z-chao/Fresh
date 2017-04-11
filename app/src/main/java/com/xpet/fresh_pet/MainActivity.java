6c4ea42f9aca8dec9f8046db47f6faa47a4a1c81
Z-chao <zanchao194@gmail.com>
2017-04-11 17:08:03 +0800

    首页基本逻辑顺利

--- app/src/main/java/com/xpet/fresh_pet/MainActivity.java
+++ app/src/main/java/com/xpet/fresh_pet/MainActivity.java
@@ -4,20 +4,34 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.text.InputType;
+import android.view.KeyEvent;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.xpet.fresh_pet.adapter.HomeAdapter;
+import com.xpet.fresh_pet.utils.GlideImageLoader;
+import com.youth.banner.Banner;
+import com.youth.banner.BannerConfig;
 
 import java.util.ArrayList;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private RecyclerView rv_main;
     private HomeAdapter mHomeAdapter;
+    private View mHeaderView;
+    private LinearLayout ll_homesearch;
+    private EditText ed_homeseach;
+    private Banner banner_home;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
         setContentView(R.layout.activity_main);
         initView();
     }
@@ -33,12 +47,57 @@ public class MainActivity extends AppCompatActivity {
         data.add("无敌神犬4");
         data.add("无敌神犬5");
         mHomeAdapter = new HomeAdapter(R.layout.item_home,data);
-        View headerView = View.inflate(MainActivity.this, R.layout.home_head, null);
-        mHomeAdapter.addHeaderView(headerView);
+        mHeaderView = View.inflate(MainActivity.this, R.layout.home_head, null);
+        initHeaderView();
+        mHomeAdapter.addHeaderView(mHeaderView);
 
         rv_main.setAdapter(mHomeAdapter);
 
+        setonListener();
 
+
+    }
+
+    private void setonListener() {
+        ll_homesearch.setOnClickListener(this);
+        ed_homeseach.setOnClickListener(this);
+
+    }
+
+    private void initHeaderView() {
+        ll_homesearch = (LinearLayout) mHeaderView.findViewById(R.id.ll_homesearch);
+        banner_home = (Banner) mHeaderView.findViewById(R.id.banner_home);
+        ed_homeseach = (EditText) mHeaderView.findViewById(R.id.ed_homeseach);
+        ed_homeseach.setInputType(InputType.TYPE_NULL);
+
+        //轮播图中数据的填充
+
+        banner_home.setImageLoader(new GlideImageLoader());
+        ArrayList<Integer> imageUrls = new ArrayList<>();
+        imageUrls.add(R.mipmap.ic_launcher);
+        imageUrls.add(R.mipmap.arrow);
+        imageUrls.add(R.mipmap.homedog);
+        banner_home.setImages(imageUrls);
+        banner_home.setIndicatorGravity(BannerConfig.LEFT);
+        banner_home.setDelayTime(1500);
+        banner_home.start();
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.ll_homesearch:
+                //搜索的逻辑
+
+                break;
+
+            case R.id.ed_homeseach:
+                //搜索
+
+                break;
+            
+        }
 
     }
 }
---
+++ app/src/main/java/com/xpet/fresh_pet/utils/GlideImageLoader.java
@@ -0,0 +1,18 @@
+package com.xpet.fresh_pet.utils;
+
+import android.content.Context;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.youth.banner.loader.ImageLoader;
+
+/**
+ * Created by D on 2017/4/10.
+ */
+
+public class GlideImageLoader extends ImageLoader {
+    @Override
+    public void displayImage(Context context, Object path, ImageView imageView) {
+        Glide.with(context).load(path).into(imageView);
+    }
+}
