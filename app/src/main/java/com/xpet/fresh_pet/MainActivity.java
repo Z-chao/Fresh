a1c20b9a5414bbe828be4397dde2982bdbc7ed6b
Z-chao <zanchao194@gmail.com>
2017-04-11 16:30:08 +0800

    首页的布局展示完毕
    如上

--- app/src/main/java/com/xpet/fresh_pet/MainActivity.java
+++ app/src/main/java/com/xpet/fresh_pet/MainActivity.java
@@ -2,11 +2,18 @@ package com.xpet.fresh_pet;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
-import android.widget.Button;
-import android.widget.Toast;
+
+import com.xpet.fresh_pet.adapter.HomeAdapter;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView rv_main;
+    private HomeAdapter mHomeAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,13 +23,22 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void initView() {
-        Button bt = (Button) findViewById(R.id.bt);
-        bt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "asdasdas", Toast.LENGTH_SHORT).show();
-            }
-        });
+        rv_main = (RecyclerView) findViewById(R.id.rv_main);
+        rv_main.setLayoutManager(new LinearLayoutManager(MainActivity.this));
+        ArrayList<String> data = new ArrayList<>();
+        data.add("无敌神犬");
+        data.add("无敌神犬1");
+        data.add("无敌神犬2");
+        data.add("无敌神犬3");
+        data.add("无敌神犬4");
+        data.add("无敌神犬5");
+        mHomeAdapter = new HomeAdapter(R.layout.item_home,data);
+        View headerView = View.inflate(MainActivity.this, R.layout.home_head, null);
+        mHomeAdapter.addHeaderView(headerView);
+
+        rv_main.setAdapter(mHomeAdapter);
+
+
 
     }
 }
---
+++ app/src/main/java/com/xpet/fresh_pet/adapter/HomeAdapter.java
@@ -0,0 +1,40 @@
+package com.xpet.fresh_pet.adapter;
+
+
+import android.widget.ImageView;
+
+import com.chad.library.adapter.base.BaseQuickAdapter;
+import com.chad.library.adapter.base.BaseViewHolder;
+import com.xpet.fresh_pet.R;
+
+
+import java.util.List;
+
+/**
+ * Created by D on 2017/4/10.
+ */
+
+public class HomeAdapter extends BaseQuickAdapter<String,BaseViewHolder> {
+    private final List<String> data;
+
+    public HomeAdapter(int layoutResId, List data) {
+        super(layoutResId, data);
+        this.data = data;
+    }
+
+
+    @Override
+    protected void convert(BaseViewHolder helper, String item) {
+        helper.setText(R.id.tv_home1, item);
+        ImageView imageView = helper.getView(R.id.iv_home1);
+
+        /*imageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Context context = view.getContext();
+                context.startActivity(new Intent(context, ListActivity.class));
+            }
+        });*/
+
+    }
+}
